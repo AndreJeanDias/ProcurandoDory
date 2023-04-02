943e05056e63fff8d8b0b376ed5a844b2690397c
AndreJeanDias <128902291+AndreJeanDias@users.noreply.github.com>
2023-04-02 17:16:01 -0300

    Feito

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Digite o texto para traduzir em baleiês:");
+        string texto = Console.ReadLine();
+
+        string textoTraduzido = TraduzirParaBaleies(texto);
+
+        Console.WriteLine("Texto traduzido em baleiês: " + textoTraduzido);
+    }
+
+    static string TraduzirParaBaleies(string texto) {
+        string textoTraduzido = "";
+
+        for (int i = 0; i < texto.Length; i++) {
+            char letra = texto[i];
+
+            if (IsVogal(letra)) {
+                textoTraduzido += letra.ToString() + letra.ToString() + letra.ToString();
+            } else {
+                textoTraduzido += letra;
+            }
+        }
+
+        return textoTraduzido;
+    }
+
+    static bool IsVogal(char letra) {
+        string vogais = "aeiouAEIOU";
+        return vogais.Contains(letra);
+    }
+}
